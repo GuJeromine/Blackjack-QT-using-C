a44efd30a6716f8f4a1a4d62b16e56a36e2f5b28
GuJeromine <107126469+GuJeromine@users.noreply.github.com>
2022-06-08 14:05:03 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <QApplication>
+#include "janela.h"
+
+int main(int argc, char* argv[]) {
+
+    QApplication app(argc, argv);
+
+    Janela j;
+
+    j.show();
+
+    return app.exec();
+
+}
