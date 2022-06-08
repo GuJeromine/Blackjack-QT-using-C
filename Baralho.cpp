32b945cd0c22081069fac4d2d4d5b49ef4124e34
GuJeromine <107126469+GuJeromine@users.noreply.github.com>
2022-06-08 15:52:16 -0300

    Update Baralho.cpp

--- Baralho.cpp
+++ Baralho.cpp
@@ -2,25 +2,36 @@
 #include <vector>
 #include <ctime>
 #include <random>
+#include <QDebug>
+Baralho* Baralho::Instancia = nullptr;
+Baralho* Baralho::getInstancia(){
+    if(Instancia == nullptr){
+        Instancia = new Baralho();
+        Instancia->EmbaralharBaralho();
+    }
+    return Instancia;
+}
 int Baralho::SortearCarta(){
 std::srand(std::time(nullptr));
-int alea = std::rand();
-alea = alea % baralho.size();
+int Aleatorio = std::rand();
+Aleatorio = Aleatorio % baralho.size();
 int carta;
-carta = baralho[alea];
+carta = baralho[Aleatorio];
 std::vector<int>::iterator it = baralho.begin();
-for(int i = 0; i < alea; i++){
+for(int i = 0; i < Aleatorio; i++){
     it++;
 }
 baralho.erase(it);
 return carta;
 }
 void Baralho::EmbaralharBaralho(){
-    Baralho::baralho = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
-                        27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52};
+    for(int i = 1; i <=52; i++){
+        baralho.push_back(i);
+    }
 }
 int Baralho::getTam(){
+    qDebug() << baralho.size();
     return baralho.size();
 }
-std::vector<int> Baralho::baralho = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
-                                     27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52};
+//std::vector<int> Baralho::baralho = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
+//                                     27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52};
